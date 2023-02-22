1a645a4753302d590e3c8e5d9db64f0cfd15c9f7
Ivo Forlin <ivo.forlin@gmail.com>
2023-02-22 19:31:30 +0100

    Fixed some lint on csv-to-timew.py

--- utils/csv-to-timew.py
+++ utils/csv-to-timew.py
@@ -1,17 +1,22 @@
+"""
+This script takes a csv file and inserts each line into timewarrior as a closed interval.
+"""
+
 import sys
 import csv
 import os
 
-if len(sys.argv) < 2:
-    raise ValueError('No file name specified')
+if __name__ == "__main__":
+  if len(sys.argv) < 2:
+      raise ValueError('No file name specified')
 
-fileToImport = sys.argv[1]
-fileToImportName = os.path.basename(fileToImport)
-trackingDate = os.path.splitext(fileToImportName)[0]
-print(f'Importing {fileToImport}')
+  file_to_import = sys.argv[1]
+  file_to_importName = os.path.basename(file_to_import)
+  tracking_date = os.path.splitext(file_to_importName)[0]
+  print(f'Importing {file_to_import}')
 
-with open(fileToImport, newline='') as csvFile:
-    timeTrackReader = csv.reader(csvFile)
-    for timeTrack in timeTrackReader:
-        command = f'timew track {trackingDate}T{timeTrack[0]} - {trackingDate}T{timeTrack[1]} {timeTrack[2]}'
-        os.system(command)
+  with open(file_to_import, newline='', encoding='UTF-8') as csvFile:
+      time_track_reader = csv.reader(csvFile)
+      for time_track in time_track_reader:
+          command = f'timew track {tracking_date}T{time_track[0]} - {tracking_date}T{time_track[1]} {time_track[2]}'
+          os.system(command)
